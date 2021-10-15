8afd0a5827502b1f57becc3e69b27ce5bb4e4ff0
dantsova <yanadontsv@gmail.com>
2021-10-15 09:18:53 +0300

    Add the ability of entering values from the command line task2.js and small clarifications task1.js

--- task1.js
+++ task1.js
@@ -1,6 +1,6 @@
 if(process.argv[2] != undefined && process.argv[3] != undefined && Number(process.argv[2]) && Number(process.argv[3])) {
-    let x = process.argv[2];
-    let y = process.argv[3];
+    let x = Number(process.argv[2]);
+    let y = Number(process.argv[3]);
 
     x=Math.sqrt(x);
     y **= 2;
--- task2.js
+++ task2.js
@@ -1,8 +1,11 @@
-let x = 2;
-let y;
-if(x>0){
-    y=2*x;
-}else{
-    y=-2*x;
+if(process.argv[2] != undefined && Number(process.argv[2])) {
+    let x = Number(process.argv[2]);
+    let y;
+    if (x > 0) {
+        y = 2 * x;
+    } else {
+        y = -2 * x;
+    }
+    console.log(`y = ${y}`);
 }
-console.log(`y = ${y}`);
+else console.error('Wrong values of arguments');
